41d93e036e71252b4df7be3e9f5deb882c11c04f
kimjay1 <kimjaypestanas@gmail.com>
2015-09-28 13:13:15 +0800

    categories-category

--- laravel/app/database/migrations/2015_09_23_031637_create_categories_table.php
+++ laravel/app/database/migrations/2015_09_23_031637_create_categories_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoryTable extends Migration {
+class CreateCategoriesTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,7 @@ class CreateCategoryTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('category', function(Blueprint $table)
+		Schema::create('categories', function(Blueprint $table)
 		{
 			$table->increments('id');
 			
