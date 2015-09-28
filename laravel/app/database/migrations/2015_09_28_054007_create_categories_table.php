d08f0ad2d0190cb5bdba30b6223e99628405c535
dombo <jansencastillo@ymail.com>
2015-09-28 13:44:48 +0800

    kiiim

--- laravel/app/database/migrations/2015_09_28_054007_create_categories_table.php
+++ laravel/app/database/migrations/2015_09_28_054007_create_categories_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoryTable extends Migration {
+class CreateCategoriesTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,12 +12,10 @@ class CreateCategoryTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('category', function(Blueprint $table)
+		Schema::create('categories', function(Blueprint $table)
 		{
 			$table->increments('id');
-			
 			$table->string('type');
-			
 			$table->timestamps();
 		});
 	}
@@ -29,7 +27,7 @@ class CreateCategoryTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('category');
+		Schema::drop('categories');
 	}
 
 }
